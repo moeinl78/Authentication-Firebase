cd2f3d9bca6ccf75aab538dde48cee05f0e77865
Moein Latifi <moeinl1090@protonmail.com>
2022-05-15 19:37:21 +0430

    you can sign in from login fragment with e/p or google

--- app/src/main/java/ir/ariyana/loginsystem/ui/fragments/LoginFragment.kt
+++ app/src/main/java/ir/ariyana/loginsystem/ui/fragments/LoginFragment.kt
@@ -1,16 +1,31 @@
 package ir.ariyana.loginsystem.ui.fragments
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.airbnb.lottie.LottieDrawable
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
+import ir.ariyana.loginsystem.R
 import ir.ariyana.loginsystem.databinding.FragmentLoginBinding
+import ir.ariyana.loginsystem.utils.Constants
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 
 class LoginFragment: Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,5 +38,85 @@ class LoginFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.fragmentLoginLottie.repeatCount = LottieDrawable.INFINITE
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.fragmentLoginConfirmButton.setOnClickListener {
+            loginUser()
+        }
+
+        binding.fragmentLoginGoogleButton.setOnClickListener {
+            googleAccountIntent()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Constants.GOOGLE_LOGIN_CODE) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data!!).result
+            account?.let { acc ->
+
+            }
+        }
+    }
+
+    // PRIVATE METHODS
+
+    private fun loginUser() {
+        val email = binding.fragmentLoginInputUsername.text.toString()
+        val password = binding.fragmentLoginInputPassword.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+
+            CoroutineScope(Dispatchers.IO).launch {
+
+                try {
+                    auth.signInWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        checkCurrentUser()
+                    }
+                }
+                catch (e: Exception) {
+                    Log.e(Constants.EXCEPTION_CONSTANT, e.message!!)
+                }
+            }
+        }
+    }
+
+    private fun googleAccountIntent() {
+        val options = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(Constants.CLIENT_ID)
+            .requestEmail()
+            .build()
+
+        val signInClient = GoogleSignIn.getClient(binding.root.context, options)
+        signInClient.signInIntent.also {
+            startActivityForResult(it, Constants.GOOGLE_LOGIN_CODE)
+        }
+    }
+
+    private fun googleAccountLogin(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main) {
+                    checkCurrentUser()
+                }
+            }
+            catch (e: Exception) {
+                Log.e(Constants.EXCEPTION_CONSTANT, e.message!!)
+            }
+        }
+    }
+
+    private fun checkCurrentUser() {
+        val user = auth.currentUser
+        if(user != null) {
+            findNavController().navigate(R.id.action_vpFragment_to_profileFragment)
+        }
     }
 }
--- app/src/main/java/ir/ariyana/loginsystem/utils/Constants.kt
+++ app/src/main/java/ir/ariyana/loginsystem/utils/Constants.kt
@@ -1,6 +1,7 @@
 package ir.ariyana.loginsystem.utils
 
 object Constants {
-
     const val EXCEPTION_CONSTANT = "error"
+    const val CLIENT_ID = "870224994105-chs9t2ggqoqjak9b0bhpb5rbcnm2cqol.apps.googleusercontent.com"
+    const val GOOGLE_LOGIN_CODE = 0
 }
