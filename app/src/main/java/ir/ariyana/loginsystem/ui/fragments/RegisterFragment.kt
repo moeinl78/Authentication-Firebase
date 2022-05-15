b50e9e0ab606168ddd266ee3a5eab1283e8730b9
Moein Latifi <moeinl1090@protonmail.com>
2022-05-15 14:47:29 +0430

    added login register & profile fragment

---
+++ app/src/main/java/ir/ariyana/loginsystem/ui/fragments/RegisterFragment.kt
@@ -0,0 +1,27 @@
+package ir.ariyana.loginsystem.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieDrawable
+import ir.ariyana.loginsystem.databinding.FragmentRegisterBinding
+
+class RegisterFragment: Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.fragmentLoginLottie.repeatCount = LottieDrawable.INFINITE
+    }
+}
